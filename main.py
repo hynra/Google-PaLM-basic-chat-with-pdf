c39fdea166f502c15bc9b027423d53022437044d
Hendra Permana <hynra@yahoo.com>
2023-10-30 15:23:34 +0700

    feat: split pdf content into chucnks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit
 from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 def main():
@@ -13,7 +14,15 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        streamlit.write(text)
+        # streamlit.write(text)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+
+        chunks = text_splitter.split_text(text=text)
+        streamlit.write(chunks)
 
 
 if __name__ == '__main__':
