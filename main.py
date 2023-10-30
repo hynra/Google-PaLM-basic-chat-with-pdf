792e720abdf651ad5c0845fe2429cbe50f22089b
Hendra Permana <hynra@yahoo.com>
2023-10-30 15:05:26 +0700

    feat: read pdf file

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import streamlit
+from PyPDF2 import PdfReader
 
 
 def main():
     streamlit.header("Basic Chat With PDF")
 
     pdf = streamlit.file_uploader("Upload a PDF File", type='pdf')
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        streamlit.write(text)
 
 
 if __name__ == '__main__':
