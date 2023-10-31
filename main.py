bed2625dfc6a9e0157d91d35bf096f554eae40c0
Hendra Permana <hynra@yahoo.com>
2023-10-31 11:05:19 +0700

    feat: save vector to local

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
-
 import streamlit
-import joblib
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
@@ -27,7 +25,6 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        # streamlit.write(text)
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200,
@@ -35,27 +32,23 @@ def main():
         )
 
         chunks = text_splitter.split_text(text=text)
-        # streamlit.write(chunks)
 
         store_name = pdf.name[:-4]
         streamlit.write(store_name)
 
         embeddings = GooglePalmEmbeddings(model_name="models/embedding-gecko-001")
-        vector_store = FAISS.from_texts(chunks, embedding=embeddings)
-        # with open(f"{store_name}.json", "w") as f:
-        #  joblib.dump(vector_store, f)
-        # joblib.dump(vector_store, f"{store_name}.joblib")
+        try:
+            vector_store = FAISS.load_local(store_name, embeddings)
+            print("Loaded vector from local")
+        except RuntimeError:
+            vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+            vector_store.save_local(store_name)
 
         query = streamlit.text_input("Ask questions about your PDF file:")
-        # streamlit.write(query)
-
         if query:
             docs = vector_store.similarity_search(query=query, k=3)
-            # streamlit.write(docs)
-
             llm = GooglePalm(model_name="models/text-bison-001")
             chain = load_qa_chain(llm=llm, chain_type="stuff")
-
             response = chain.run(input_documents=docs, question=query)
             streamlit.write(response)
 
